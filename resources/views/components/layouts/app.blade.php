d916d4718d545415ce11e1bb32d76bf8c907aeea
Ilya Servetnik <iservetnik.ufo@gmail.com>
2024-04-27 21:34:55 +0300

    Installed livewire

---
+++ resources/views/components/layouts/app.blade.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>VNTU</title>
+
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+    @livewireStyles
+</head>
+
+<body>
+    <div>
+        <x-desktop-sidebar />
+        <x-mobile-sidebar />
+
+        <main class="py-10 lg:pl-72">
+            <div class="px-4 sm:px-6 lg:px-8">
+                {{ $slot }}
+            </div>
+        </main>
+    </div>
+
+    @livewireScripts
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,14 @@
 <?php
 
+use App\Livewire\Clients\ClientCreate;
+use App\Livewire\Clients\ClientEdit;
+use App\Livewire\Clients\ClientsIndex;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+Route::view('/', 'welcome');
 
-Route::get('/', function () {
-    return view('welcome');
+Route::prefix('clients')->name('clients.')->group(function () {
+    Route::get('/', ClientsIndex::class)->name('index');
+    Route::get('create', ClientCreate::class)->name('create');
+    Route::get('{full_name}/edit', ClientEdit::class)->name('edit');
 });
