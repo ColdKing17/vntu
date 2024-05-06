1e1086bcb4975d50c8d978ee684f90d9d3f83d58
ColdKing17 <ilaservetnik@gmail.com>
2024-05-06 19:01:10 +0300

    Removed seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,26 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            StreetSeeder::class,
-            CitySeeder::class,
-            BuilderSeeder::class,
-            AdvertisingCampaignSeeder::class,
-            ServiceSeeder::class,
-            ClientSeeder::class,
-            RequestSeeder::class,
-            RealtorSeeder::class,
-            DepartmentOfficeSeeder::class,
-            DepartmentSeeder::class,
-            DistrictSeeder::class,
-            ResidentialComplexSeeder::class,
-            RealEstateSeeder::class,
-            PaymentSeeder::class,
-            RequestClientSeeder::class,
-            RequestRealtorSeeder::class,
-            DepartmentAdvertisingCampaignSeeder::class,
-            DistrictCitySeeder::class,
-            RealtorDepartmentServiceSeeder::class,
-            PaymentRequestSeeder::class,
+            //
         ]);
     }
 }
